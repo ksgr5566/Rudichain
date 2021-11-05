49362612630ec408e3a436810ba473e0c6d221c7
ksgr5566 <ksgr5566@gmail.com>
2021-11-05 20:52:24 +0530

    combining firstbranch and init springboot

---
+++ rudichain/src/main/java/com/rudichain/rudichain/constants.java
@@ -0,0 +1,12 @@
+package com.rudichain.rudichain;
+
+public enum constants{
+    INITIAL_DIFFICULTY(3), MINE_RATE(1000);
+    private int p;
+    constants(int p){
+        this.p = p;
+    }
+    public int getValue(){
+        return p;
+    }
+}
