16b9e67807d944329dac83946a74bc2ff32bc492
ksgr5566 <ksgr5566@gmail.com>
2021-11-08 11:35:52 +0530

    Squashed commit of the following:
    commit 8d6eb03a0d5d718e52588ad2f7b24658ef4fd63d
    Author: ksgr5566 <ksgr5566@gmail.com>
    Date:   Sat Nov 6 23:43:55 2021 +0530
    
        sec-5 completed
    
    commit 15634c4db4a6009775c2571c171e94b12e12a3a4
    Author: ksgr5566 <ksgr5566@gmail.com>
    Date:   Sat Nov 6 18:52:12 2021 +0530
    
        removed errors from pubsub and fully functioning
    
        fully functioning created with this commit. create a new branch for further commits
    
    commit 6c4ef7b2625771b2a11ee27b42b632d899be439d
    Author: ksgr5566 <ksgr5566@gmail.com>
    Date:   Sat Nov 6 00:08:30 2021 +0530
    
        index.java integration of pubsub
    
        still needs to implement full functionality
    
    commit 037ecd4daceb0b441b4cdf6353fbf88e1ff1e4f4
    Author: ksgr5566 <ksgr5566@gmail.com>
    Date:   Fri Nov 5 22:18:42 2021 +0530
    
        first commit of index.java
    
        working for basic functions: get and post. pubsub yet to be implemented
    
    commit 49362612630ec408e3a436810ba473e0c6d221c7
    Author: ksgr5566 <ksgr5566@gmail.com>
    Date:   Fri Nov 5 20:52:24 2021 +0530
    
        combining firstbranch and init springboot
    
    commit a3210fbeb31773b699519f9786a2ae76762cf8ce
    Author: ksgr5566 <ksgr5566@gmail.com>
    Date:   Fri Nov 5 20:35:49 2021 +0530
    
        initial springboot commit of Rudichain
    
    commit 47498f633ce937369adab8ed8e624801f33701ae
    Author: ksgr5566 <ksgr5566@gmail.com>
    Date:   Fri Nov 5 20:34:13 2021 +0530
    
        deleting previous versions
    
    commit ec23c6188c0f2077216e5c6ca3c152dfa1bb01f1
    Merge: aa1dc21 83e9c23
    Author: ksgr5566 <ksgr5566@gmail.com>
    Date:   Fri Nov 5 14:41:13 2021 +0530
    
        Merge branch 'first_branch' of https://github.com/ksgr5566/Rudichain into first_branch
    
    commit aa1dc210c25bb58c423be2c69fa5db5f91320748
    Author: ksgr5566 <ksgr5566@gmail.com>
    Date:   Fri Nov 5 14:40:37 2021 +0530
    
        removed errors from initial pubsub
    
        still  not finished.. future commits should be made to complete this file
    
    commit 83e9c2334719053cb6925f9ae22ceb56f7e4192f
    Merge: 409c933 0eaef17
    Author: appala venkata avinash <aa760@snu.edu.in>
    Date:   Fri Nov 5 14:35:01 2021 +0530
    
        Merge branch 'first_branch' of https://github.com/ksgr5566/Rudichain into first_branch
    
    commit 409c93382251f36fb461b112dacd4093195b798e
    Author: appala venkata avinash <aa760@snu.edu.in>
    Date:   Fri Nov 5 14:26:36 2021 +0530
    
        removed errors from pubsub.java
    
    commit 0eaef1769c19d25425f6e11878542cf2f8f5c78a
    Author: ksgr5566 <ksgr5566@gmail.com>
    Date:   Fri Nov 5 14:23:02 2021 +0530
    
        adding pubsub.java
    
        there are a few errors which needs to be fixed
    
    commit 59245d73bc6b01f74b4882333100475de252444d
    Author: ksgr5566 <ksgr5566@gmail.com>
    Date:   Thu Nov 4 21:33:26 2021 +0530
    
        adding blockchain.java
    
        still have to complete but compiles fine with no errors
    
    commit 5a51dc69fca04559434ba49291721b87fc60fa6c
    Author: TheSaviour <saranbodduluri@gmail.com>
    Date:   Thu Nov 4 19:10:40 2021 +0530
    
        blockchain.java initial commit.
    
        lot of space for improvement.
    
    commit 8930d4d735e23fc141a368d1341b57f44d499449
    Author: ksgr5566 <ksgr5566@gmail.com>
    Date:   Thu Nov 4 18:00:04 2021 +0530
    
        deleting unnecessary files
    
    commit bb7148309e906f3ece1b774bdb3dff585328cd62
    Author: ksgr5566 <ksgr5566@gmail.com>
    Date:   Thu Nov 4 17:56:42 2021 +0530
    
        finished block.java
    
        added dependency javax.xml.bind, added hash.java, constants.java ; and completed block.java
    
    commit db784ef04a9b871a0ee93e6745a66f4b2ae40b42
    Author: ksgr5566 <ksgr5566@gmail.com>
    Date:   Thu Nov 4 13:59:12 2021 +0530
    
        created backend folder
    
    commit 959ea7ce42468d06e2b6f1c2e9896604321a1839
    Author: TheSaviour <saranbodduluri@gmail.com>
    Date:   Thu Nov 4 13:51:39 2021 +0530
    
        testfile
    
        testing

---
+++ rudichain/src/main/java/com/rudichain/rudichain/index.java
@@ -0,0 +1,42 @@
+package com.rudichain.rudichain;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.google.gson.Gson; 
+
+import com.rudichain.rudichain.backend.blockchain;
+import com.rudichain.rudichain.network.pubsub;
+
+@RestController
+
+public class index{
+
+    blockchain chain = new blockchain();
+    pubsub client = new pubsub(chain);
+
+    @RequestMapping("/api/blocks")
+    @GetMapping()
+    public String RespondWithChain(){
+        Gson gson = new Gson();
+        client.broadcastChain();
+        return gson.toJson(chain.chain);
+    }
+
+    @RequestMapping("/api/mine")
+    @PostMapping()
+    public ModelAndView PostData(@RequestBody String data){
+        chain.addBlock(data);
+        client.broadcastChain();
+        System.out.println("Redirecting to /api/blocks....");
+        return new ModelAndView("redirect:/api/blocks");
+    }
+
+    //to be thorouly tested
+
+    
+}
---
+++ rudichain/src/test/java/com/rudichain/rudichain/RudichainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rudichain.rudichain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RudichainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
